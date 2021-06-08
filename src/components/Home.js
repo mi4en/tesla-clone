04af1af42bb6a1f20c1697757acb39e035c29daa
mnikolov-qb <83956234+mnikolov-qb@users.noreply.github.com>
2021-06-08 09:06:18 +0300

    add sections;

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,15 +4,61 @@ import styled from 'styled-components'
 import Section from './Section'
 
 const Home = () => {
-	return (
-		<Container>
-			<Section />
-		</Container>
-	)
+  return (
+    <Container>
+      <Section
+        title='Model S'
+        desc='Order Online for Touchless Delivery'
+        leftBtnTxt='Custom Order'
+        rightBtnTxt='Existing Inventory'
+        bgImg='model-s.jpg'
+      />
+      <Section
+        title='Model Y'
+        desc='Order Online for Touchless Delivery'
+        leftBtnTxt='Custom Order'
+        rightBtnTxt='Existing Inventory'
+        bgImg='model-y.jpg'
+      />
+      <Section
+        title='Model 3'
+        desc='Order Online for Touchless Delivery'
+        leftBtnTxt='Custom Order'
+        rightBtnTxt='Existing Inventory'
+        bgImg='model-3.jpg'
+      />
+      <Section
+        title='Model X'
+        desc='Order Online for Touchless Delivery'
+        leftBtnTxt='Custom Order'
+        rightBtnTxt='Existing Inventory'
+        bgImg='model-x.jpg'
+      />
+      <Section
+        title='Lowest Cost Solar Panels in America'
+        desc='Money-back Guarantee'
+        bgImg='solar-panel.jpg'
+        leftBtnTxt='Order Now'
+        rightBtnTxt='Learn More'
+      />
+      <Section
+        title='Solars for New Roofs'
+        desc='Solar Roof Costs Less Than a New Roof'
+        bgImg='solar-roof.jpg'
+        leftBtnTxt='Order Now'
+        rightBtnTxt='Learn More'
+      />
+      <Section
+        title='Accessories'
+        bgImg='accessories.jpg'
+        leftBtnTxt='Shop Now'
+      />
+    </Container>
+  )
 }
 
 export default Home
 
 const Container = styled.div`
-	height: 100vh;
+  height: 100vh;
 `
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,21 +1,86 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Section = () => {
-	return (
-		<Wrap>
-			<h3>Hi, I am section</h3>
-		</Wrap>
-	)
+const Section = ({ title, desc, bgImg, leftBtnTxt, rightBtnTxt }) => {
+  return (
+    <Wrap bgImg={bgImg}>
+      <ItemText>
+        <h1>{title}</h1>
+        <p>{desc}</p>
+      </ItemText>
+
+      <Buttons>
+        <BtnGroup>
+          <LeftBtn>{leftBtnTxt}</LeftBtn>
+          {rightBtnTxt && <RightBtn>{rightBtnTxt}</RightBtn>}
+        </BtnGroup>
+
+        <DownArrow src='/images/down-arrow.svg'></DownArrow>
+      </Buttons>
+    </Wrap>
+  )
 }
 
 export default Section
 
 const Wrap = styled.div`
-	width: 100vw;
-	height: 100vh;
-	background-position: center;
-	background-size: cover;
-	background-repeat: no-repeat;
-	background-image: url('/images/model-s.jpg');
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: ${({ bgImg }) => `url('images/${bgImg}')`};
 `
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`
+
+const Buttons = styled.div``
+
+const BtnGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`
+
+const LeftBtn = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`
+
+const RightBtn = styled(LeftBtn)`
+  background-color: #fff;
+  opacity: 0.65;
+  color: #333;
+`
+
+const DownArrow = styled.img`
+  height: 40px;
+  overflow-x: hidden;
+  animation: animateDown infinite 1.5s;
+`
+
+// Section.defaultProps = {
+//   desc: 'Order Online for Touchless Delivery',
+//   leftBtnTxt: 'Custom Order',
+//   rightBtnTxt: 'Existing Inventory',
+// }
