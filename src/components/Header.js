d79c1c5a9e112e61451d0579b910acaa8ce1ece8
mnikolov-qb <83956234+mnikolov-qb@users.noreply.github.com>
2021-06-08 11:13:09 +0300

    add redux for cars;

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
 import styled from 'styled-components'
 
 import MenuIcon from '@material-ui/icons/Menu'
+import CloseIcon from '@material-ui/icons/Close'
+import { selectCars } from '../features/car/carSlice'
 
 const Header = () => {
+  const [showBurgerNav, setShowBurgerNav] = useState(false)
+
+  const cars = useSelector(selectCars)
+
   return (
     <Container>
       <a href='tesla.com'>
         <img src='/images/logo.svg' alt='' />
       </a>
       <Menu>
-        <a href='#'>Model S</a>
-        <a href='#'>Model Y</a>
-        <a href='#'>Model 3</a>
-        <a href='#'>Model X</a>
+        {cars &&
+          cars.map((car, index) => (
+            <a key={index} href='#'>
+              {car}
+            </a>
+          ))}
       </Menu>
 
       <RightMenu>
         <a href='#'>Shop</a>
         <a href='#'>Tesla Account</a>
 
-        <CustomMenu></CustomMenu>
+        <CustomMenu onClick={() => setShowBurgerNav(true)} />
       </RightMenu>
+
+      <BurgerNav show={showBurgerNav}>
+        <CloseWrapper>
+          <CustomClose onClick={() => setShowBurgerNav(false)} />
+        </CloseWrapper>
+        <li>
+          <a href='#'>Existing Inventory</a>
+        </li>
+        <li>
+          <a href='#'>Used Inventory</a>
+        </li>
+        <li>
+          <a href='#'>Trade-in</a>
+        </li>
+        {cars &&
+          cars.map((car, index) => (
+            <li>
+              <a key={index} href='#'>
+                {car}
+              </a>
+            </li>
+          ))}
+        <li>
+          <a href='#'>Cyber Truck</a>
+        </li>
+        <li>
+          <a href='#'>Roadster</a>
+        </li>
+        <li>
+          <a href='#'>Semi</a>
+        </li>
+        <li>
+          <a href='#'>Warranty</a>
+        </li>
+      </BurgerNav>
     </Container>
   )
 }
@@ -38,6 +82,7 @@ const Container = styled.div`
   top: 0;
   left: 0;
   right: 0;
+  z-index: 1;
 `
 
 const Menu = styled.div`
@@ -67,5 +112,36 @@ const RightMenu = styled.div`
 `
 
 const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
+`
+
+const BurgerNav = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  text-align: start;
+  background: #fff;
+  width: 300px;
+  list-style: none;
+  padding: 20px;
+  z-index: 2;
+  transform: ${({ show }) => (show ? 'translateX(0)' : 'translateX(100%)')};
+  transition: transform 0.3s ease-in-out;
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+    a {
+      font-weight: 600;
+    }
+  }
+`
+
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`
+
+const CustomClose = styled(CloseIcon)`
   cursor: pointer;
 `
